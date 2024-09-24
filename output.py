aafd151262b9956071e76628ff7448558b026c90
action <action@github.com>
2024-09-24 03:37:18 +0000

    Add decoded output file

--- output.py
+++ output.py
@@ -1,65 +1,160 @@
-#2024-09-24 03:33:23
-import requests
+#2024-09-24 03:37:11
+
 import os
-import time
 import random
-import hashlib
-class yuanshen():
- def __init__(self,cookie):
-  self.cookie=cookie
-  self.h={"Host":"app.zhuanbang.net","accept":"application/json, image/webp","user-agent":"Mozilla/5.0 (Linux; Android 12; M2104K10AC Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/96.0.4664.104 Mobile Safari/537.36 HuoNiuFusion/1.25.0_231652","x-requested-with":"XMLHttpRequest","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-dest":"empty","referer":"https://app.zhuanbang.net/assist/activity/47","accept-language":"zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7","accept-encoding":"gzip","Cookie":f"NiuToken={self.cookie}"}
- def sign_(self):
-  d=f"{self.csrftoken}#{self.sessionId}#{self.time}"
-  byte_string=d.encode('utf-8')
-  sha1=hashlib.sha1()
-  sha1.update(byte_string)
-  sign=sha1.hexdigest()
-  return sign
- def video(self,key):
-  i=0
-  while True:
-   i+=1
-   url=f"https://app.zhuanbang.net/{key}/launch?_random={int(time.time() * 1000)}&type=slide"
-   r=requests.get(url,headers=self.h).json()
-   if r['code']==0:
-    print(f"第[{i}]个红包获取信息成功")
-    self.csrftoken=r['data']['extArgs']['csrfToken']
-    self.sessionId=r['data']['extArgs']['sessionId']
-    self.time=int(time.time())
-    url=f"https://app.zhuanbang.net/{key}/award/grant?_t={self.time}"
-    data={"csrfToken":f"{self.csrftoken}","deviceId":f"{self.sessionId}","timestamp":f"{self.time}","sign":f"{self.sign_()}"}
-    r=requests.post(url,headers=self.h,data=data).json()
-    if r['code']==0:
-     print(f"第[{i}]个红包领取成功,获得[{r['data']['awardMoney']}]元")
-    else:
-     print(f"第[{i}]个红包领取失败---[{r['msg']}]")
-     break
-   else:
-    print(f"第[{i}]个获取红包信息失败---[{r['msg']}]")
-    break
-   if i>=21:
-    break
-   time.sleep(random.randint(20,48))
- def main(self):
-  print("===========开始执行快手刷视频===========")
-  self.video("kwai_video")
-  print("===========快手刷视频执行完毕===========")
-  print("===========开始执行抖音刷视频===========")
-  self.video("pangle_video")
-  print("===========抖音刷视频执行完毕===========")
-if __name__=='__main__':
- cookie=''
- if not cookie:
-  cookie=os.getenv("yuanshen_zb")
-  if not cookie:
-   print("⛔️请设置环境变量:yuanshen_zb")
-   exit()
- cookies=cookie.split("@")
- print(f"一共获取到{len(cookies)}个账号")
- i=1
- for cookie in cookies:
-  print(f"\n--------开始第{i}个账号--------")
-  main=yuanshen(cookie)
-  main.main()
-  print(f"--------第{i}个账号执行完毕--------")
-  i+=1
+import requests
+import time
+
+accounts = os.getenv("xpnc", "").splitlines()
+print("☞☞☞ 兴攀农场每日签到 ☜☜☜")
+
+
+def handle(headers):
+    try:
+        types = [1, 2, 3]
+        for type in types:
+            getTasks(headers, type)
+        addWaterFertilizer(headers)
+    except requests.RequestException as e:
+        print(f"{note.strip()}：响应 {e}")
+
+
+def getTasks(headers, type):
+    url = "https://p.xpfarm.cn/treemp/tree.Tasks/getTasks"
+    params = {"type": type}
+    try:
+        response = requests.post(url, headers=headers, params=params)
+        print(f"获取{note.strip()}任务列表")
+        resJson = response.json()
+        if resJson['code'] != 1000:
+            print(f"{note.strip()}：获取任务列表失败")
+            raise Exception
+        data = resJson['data']
+        for task in data:
+            if task['is_completed'] == 1:
+                print(f"{note.strip()}：任务 {task['name']} 已完成")
+                receiveTaskReward(headers, task)
+                break
+            elif task['work_type'] == 1:
+                time.sleep(random.randint(20, 30))
+                completeTask(headers, task)
+            elif task['work_type'] == 2:
+                time.sleep(random.randint(1, 3))
+                completeTask(headers, task)
+            elif task['work_type'] == 4:
+                time.sleep(random.randint(1, 3))
+                completeTask(headers, task)
+            else:
+                print(f"{note.strip()}：任务 {task['name']} 不支持")
+    except requests.RequestException as e:
+        print(f"{note.strip()}：响应 {e}")
+
+
+def addWaterFertilizer(headers):
+    homeData = getHomePage(headers)
+    if homeData['water_value'] > 0:
+        for i in range(1, 100):
+            time.sleep(random.randint(2, 4))
+            url = "https://p.xpfarm.cn/treemp/tree.Tasks/addWater"
+            try:
+                response = requests.post(url, headers=headers)
+                print(f"{note.strip()} 第 {i} 次浇水")
+                resJson = response.json()
+                if resJson['code'] != 1000:
+                    print(f"{note.strip()}：{resJson['message']}")
+                    break
+            except requests.RequestException as e:
+                print(f"{note.strip()}：响应 {e}")
+    if homeData['fertilizer'] > 0:
+        for i in range(1, 100):
+            time.sleep(random.randint(2, 4))
+            url = "https://p.xpfarm.cn/treemp/tree.Tasks/addFertilizer"
+            try:
+                response = requests.post(url, headers=headers)
+                print(f"{note.strip()} 第 {i} 次施肥")
+                resJson = response.json()
+                if resJson['code'] != 1000:
+                    print(f"{note.strip()}：{resJson['message']}")
+                    break
+            except requests.RequestException as e:
+                print(f"{note.strip()}：响应 {e}")
+    if homeData['integral'] >= 0:
+        print(f"{note.strip()}：当前积分 {homeData['integral']}")
+
+
+def getHomePage(headers):
+    url = "https://p.xpfarm.cn/treemp/tree.Tasks/getHomePage"
+    params = {
+    }
+    try:
+        response = requests.post(url, headers=headers, params=params)
+        print(f"{note.strip()} 获取个人信息")
+        resJson = response.json()
+        if resJson['code'] != 1000:
+            raise Exception
+        return resJson['data']
+    except requests.RequestException as e:
+        print(f"{note.strip()}：响应 {e}")
+
+
+def completeTask(headers, task):
+    url = "https://p.xpfarm.cn/treemp/tree.Tasks/completeTask"
+    params = {"task_id": task['id']}
+    try:
+        response = requests.post(url, headers=headers, params=params)
+        resJson = response.json()
+        if resJson['code'] == 1000:
+            print(f"{note.strip()}：{task['name']} 完成")
+            receiveTaskReward(headers, task)
+        else:
+            print(f"{note.strip()}：{task['name']} {resJson['message']}")
+    except requests.RequestException as e:
+        print(f"{note.strip()}：{task['name']}响应 {e}")
+
+
+def receiveTaskReward(headers, task):
+    url = "https://p.xpfarm.cn/treemp/tree.Tasks/receiveTaskReward"
+    params = {"task_id": task['id']}
+    try:
+        response = requests.post(url, headers=headers, params=params)
+        resJson = response.json()
+        print(resJson)
+        if resJson['code'] == 1000:
+            reward = resJson['data']['reward'][0]
+            print(f"{note.strip()}：任务 {task['name']} 领取{reward['reward_type_name']}-{reward['reward']}")
+        else:
+            print(f"{note.strip()}：任务 {task['name']} {resJson['message']}")
+    except requests.RequestException as e:
+        print(f"{note.strip()}：{task['name']}响应 {e}")
+
+
+if not accounts:
+    print("未找到任何账号信息。")
+else:
+    for account in accounts:
+        if not account.strip():
+            continue
+        try:
+            token, note = account.split("#")
+        except ValueError:
+            print(f"格式错误: {account}")
+            continue
+
+        url = "https://p.xpfarm.cn/treemp/user.PersonalCenter/addSignIn"
+        headers = {
+            'User-Agent': "Mozilla/5.0 (Linux; Android 10; Mobile Safari/537.36)",
+            'Accept-Encoding': "gzip, deflate, br",
+            'Content-Type': "application/json",
+            'authorization': token.strip()
+        }
+        try:
+            response = requests.post(url, headers=headers)
+            print(f"{note.strip()}：响应 {response.text}")
+            resJson = response.json()
+            if resJson['code'] == 1000:
+                print(f"{note.strip()}：签到成功，获得积分 {resJson['data']['reward']['integral']}")
+            else:
+                print(f"{note.strip()}：响应 {resJson['message']}")
+            handle(headers)
+        except requests.RequestException as e:
+            print(f"{note.strip()}：请求失败 - {e}")
