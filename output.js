a0bcbd1e5c212c8da09c99c97b8588a4fa46eb92
action <action@github.com>
2024-11-20 06:33:56 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Sat Nov 16 2024 02:47:48 GMT+0000 (Coordinated Universal Time)
+//Wed Nov 20 2024 06:33:56 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("移动心愿金"),
