06d7381b6fa6e1d1c91cce8b815130bb073cda34
lihailong <158006169+lihailong1995@users.noreply.github.com>
2025-09-15 17:15:51 +0800

    Update input.js

--- input.js
+++ input.js
@@ -1,1 +1,205 @@
-var _0x59d17a=_0xdcb1;(function(_0xa34ddc,_0x28f7f9){var _0xfa546e=_0xdcb1,_0x12c213=_0xa34ddc();while(!![]){try{var _0x48c032=parseInt(_0xfa546e(0x161))/0x1+parseInt(_0xfa546e(0x17b))/0x2*(-parseInt(_0xfa546e(0x170))/0x3)+parseInt(_0xfa546e(0x162))/0x4*(-parseInt(_0xfa546e(0x152))/0x5)+parseInt(_0xfa546e(0x156))/0x6+-parseInt(_0xfa546e(0x181))/0x7*(-parseInt(_0xfa546e(0x179))/0x8)+parseInt(_0xfa546e(0x180))/0x9*(parseInt(_0xfa546e(0x173))/0xa)+parseInt(_0xfa546e(0x17d))/0xb*(-parseInt(_0xfa546e(0x159))/0xc);if(_0x48c032===_0x28f7f9)break;else _0x12c213['push'](_0x12c213['shift']());}catch(_0x260833){_0x12c213['push'](_0x12c213['shift']());}}}(_0x5207,0x3d57a));var a=[_0x59d17a(0x160),_0x59d17a(0x164),_0x59d17a(0x176),_0x59d17a(0x15c),_0x59d17a(0x17c),'Gks8Kg==',_0x59d17a(0x172)];(function(_0x396532,_0x21628e,_0x730234){var _0x4f76c9=(function(){var _0x1e1f56=!![];return function(_0x2f3d4b,_0x41bc89){var _0x1dee32=_0x1e1f56?function(){var _0xc0a399=_0xdcb1;if(_0x41bc89){var _0xd4b7eb=_0x41bc89[_0xc0a399(0x167)](_0x2f3d4b,arguments);return _0x41bc89=null,_0xd4b7eb;}}:function(){};return _0x1e1f56=![],_0x1dee32;};}()),_0x328738=function(_0x326b14,_0xf69767,_0x56667a,_0x2fbbd6){var _0x338c8e=_0xdcb1,_0x1e3445=_0x4f76c9(this,function(){var _0x116486=_0xdcb1;return _0x1e3445['toString']()[_0x116486(0x16d)](_0x116486(0x169))[_0x116486(0x15e)]()[_0x116486(0x16b)](_0x1e3445)['search'](_0x116486(0x169));});_0x1e3445(),_0xf69767=_0xf69767>>0x8;if(_0xf69767<_0x326b14){while(--_0x326b14){_0x2fbbd6=_0x396532[_0x338c8e(0x153)]();if(_0xf69767===_0x326b14)_0xf69767=_0x2fbbd6,_0x56667a=_0x396532['shift']();else _0x56667a[_0x338c8e(0x163)](/[ZglBUDOPlrwEVBpl=]/g,'')===_0xf69767&&_0x396532[_0x338c8e(0x17f)](_0x2fbbd6);}_0x396532[_0x338c8e(0x17f)](_0x396532[_0x338c8e(0x153)]());}return 0x242ed;};return _0x328738(++_0x21628e,_0x730234)>>_0x21628e^_0x730234;}(a,0x1eb,0x1eb00));function _0xdcb1(_0x1e2c71,_0x7b05db){var _0xd4be99=_0x5207();return _0xdcb1=function(_0x11d5bf,_0x227921){_0x11d5bf=_0x11d5bf-0x152;var _0x520748=_0xd4be99[_0x11d5bf];return _0x520748;},_0xdcb1(_0x1e2c71,_0x7b05db);}var b=function(_0x15b476,_0x56aaf8){var _0x3c6a08=_0x59d17a;_0x15b476=~~'0x'[_0x3c6a08(0x16e)](_0x15b476);var _0xfec96a=a[_0x15b476];if(b['gPvZlv']===undefined){(function(){var _0x574ec1=_0x3c6a08,_0x5543df=typeof window!==_0x574ec1(0x177)?window:typeof process===_0x574ec1(0x165)&&typeof require===_0x574ec1(0x171)&&typeof global===_0x574ec1(0x165)?global:this,_0x2639b3=_0x574ec1(0x155);_0x5543df[_0x574ec1(0x174)]||(_0x5543df['atob']=function(_0x39b971){var _0x45032d=_0x574ec1,_0x44f198=String(_0x39b971)[_0x45032d(0x163)](/=+$/,'');for(var _0x48f2fc=0x0,_0x15031b,_0x30e1f2,_0x2f6007=0x0,_0x4023bd='';_0x30e1f2=_0x44f198['charAt'](_0x2f6007++);~_0x30e1f2&&(_0x15031b=_0x48f2fc%0x4?_0x15031b*0x40+_0x30e1f2:_0x30e1f2,_0x48f2fc++%0x4)?_0x4023bd+=String['fromCharCode'](0xff&_0x15031b>>(-0x2*_0x48f2fc&0x6)):0x0){_0x30e1f2=_0x2639b3[_0x45032d(0x15f)](_0x30e1f2);}return _0x4023bd;});}());var _0x514536=function(_0x3fe201,_0x2e11f5){var _0x4df2bd=_0x3c6a08,_0x3d43e1=[],_0x5a2588=0x0,_0x3a3f93,_0x5766a1='',_0x2cbb8f='';_0x3fe201=atob(_0x3fe201);for(var _0x43f961=0x0,_0x25b2e4=_0x3fe201['length'];_0x43f961<_0x25b2e4;_0x43f961++){_0x2cbb8f+='%'+('00'+_0x3fe201['charCodeAt'](_0x43f961)[_0x4df2bd(0x15e)](0x10))[_0x4df2bd(0x15a)](-0x2);}_0x3fe201=decodeURIComponent(_0x2cbb8f);for(var _0x55ec82=0x0;_0x55ec82<0x100;_0x55ec82++){_0x3d43e1[_0x55ec82]=_0x55ec82;}for(_0x55ec82=0x0;_0x55ec82<0x100;_0x55ec82++){_0x5a2588=(_0x5a2588+_0x3d43e1[_0x55ec82]+_0x2e11f5[_0x4df2bd(0x157)](_0x55ec82%_0x2e11f5[_0x4df2bd(0x178)]))%0x100,_0x3a3f93=_0x3d43e1[_0x55ec82],_0x3d43e1[_0x55ec82]=_0x3d43e1[_0x5a2588],_0x3d43e1[_0x5a2588]=_0x3a3f93;}_0x55ec82=0x0,_0x5a2588=0x0;for(var _0x2d3d4b=0x0;_0x2d3d4b<_0x3fe201[_0x4df2bd(0x178)];_0x2d3d4b++){_0x55ec82=(_0x55ec82+0x1)%0x100,_0x5a2588=(_0x5a2588+_0x3d43e1[_0x55ec82])%0x100,_0x3a3f93=_0x3d43e1[_0x55ec82],_0x3d43e1[_0x55ec82]=_0x3d43e1[_0x5a2588],_0x3d43e1[_0x5a2588]=_0x3a3f93,_0x5766a1+=String[_0x4df2bd(0x168)](_0x3fe201[_0x4df2bd(0x157)](_0x2d3d4b)^_0x3d43e1[(_0x3d43e1[_0x55ec82]+_0x3d43e1[_0x5a2588])%0x100]);}return _0x5766a1;};b[_0x3c6a08(0x16a)]=_0x514536,b['uzmgkx']={},b[_0x3c6a08(0x16f)]=!![];}var _0x194b2f=b['uzmgkx'][_0x15b476];return _0x194b2f===undefined?(b['GkZZBA']===undefined&&(b[_0x3c6a08(0x15b)]=!![]),_0xfec96a=b[_0x3c6a08(0x16a)](_0xfec96a,_0x56aaf8),b[_0x3c6a08(0x16c)][_0x15b476]=_0xfec96a):_0xfec96a=_0x194b2f,_0xfec96a;};(function(_0x121874,_0xd00d5c){var _0x1087ee=_0x59d17a,_0x181d6f={'RAMoq':_0x1087ee(0x15d),'EaZaN':_0x1087ee(0x158)};_0x121874[b('0',_0x1087ee(0x175))]=_0x181d6f[b('1',_0x1087ee(0x17a))],_0xd00d5c[b('2',_0x1087ee(0x17a))]=_0x181d6f[b('3',_0x1087ee(0x166))],_0xd00d5c[_0x1087ee(0x17e)]=b('4',_0x1087ee(0x154));}(window,document));function _0x5207(){var _0x27edf0=['156oFuFVQ','replace','ZjsgjilBUDamOPlriw.EVcomBp.lv6==','object','jPNw','apply','fromCharCode','(((.+)+)+)+$','fuFDUP','constructor','uzmgkx','search','concat','gPvZlv','3zPWuKF','function','w4h1w7oOag==','4607330joTDKv','atob','ZdTU','5p+76Zev6aOL6K2W6ICh57Czw59KXsO7woIUw6fCgMKLw5TCqg==','undefined','length','8WZbqlE','FnxY','229234vKhunt','IWQXKgw=','6093494yvFljo','feedback','push','9UWJRWB','1024093MgAOIW','52835WLbxNw','shift','fS%!','ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=','2115102EeaaZj','charCodeAt','这个是一个本站对JavaScript\x20脚本的一个最牛加密，兼容性适中，解密难度极大','12lrzukB','slice','GkZZBA','KFXCvcKCJFM=','2019年09月12日15:24:29更新','toString','indexOf','jsjiami.com.v6','372393kEMvLw'];_0x5207=function(){return _0x27edf0;};return _0x5207();}
+const $ = new Env('DDNSTO自动续期');
+let ddnstock = ($.isNode() ? process.env.ddnstock : $.getdata('ddnstock')) || '';
+let ddnstockArr = []
+let csrftoken = ($.isNode() ? process.env.csrftoken : $.getdata('csrftoken')) || '';
+let csrftokenArr = []
+var request = require('request');
+
+
+
+
+!(async () => {
+  if(ddnstock) {
+    ddnstockArr = ddnstock.split('@')
+} else {
+    console.log('未找到ddnstock\n')
+    return false
+}
+if(csrftoken) {
+  csrftokenArr = csrftoken.split('@')
+} else {
+  console.log('未找到csrftoken\n')
+  return false
+}
+await ica();
+await $.wait(2000);
+await uid();
+await $.wait(2000);
+   await uu();
+   await $.wait(2000);
+   await ib();
+})()
+
+
+function generateUUID() {
+  let d = new Date().getTime();
+  
+  let uuid = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      let r = (d + Math.random() * 16) % 16 | 0;
+      d = Math.floor(d / 16);
+      return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  });
+  return uuid;
+}
+
+
+
+
+async function uu(){
+var headers = {
+    'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "HuaweiBrowser";v="92"',
+    'authority': 'www.ddnsto.com',
+    'accept': 'application/json, text/plain, */*',
+    'x-csrftoken': csrftoken,
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+    'content-type': 'application/json',
+    'origin': 'https://www.ddnsto.com',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'referer': 'https://www.ddnsto.com/app/',
+    'accept-language': 'zh-CN,zh;q=0.9',
+    'cookie': ddnstock
+};
+
+var dataString = '{"product_id":2,"uuid_from_client":"'+generateUUID()+'"}';
+
+var options = {
+    url: 'https://www.ddnsto.com/api/user/product/orders/',
+    method: 'POST',
+    headers: headers,
+    body: dataString
+};
+
+function callback(error, response, body) {
+  
+  
+  
+  data = JSON.parse(body)
+    if (data.product.id == 2) {
+     
+      //cjsj = data.created_at
+     // gxsj =data.updated_at
+     // dqsj = data.product_expired_at
+     // dqrz = data.product_expired_at
+     // dglx = '4Mbps 7天试用'
+      id = data.id
+
+      //  console.log(id+'订购ddnsto：'+dglx+'\n创建时间：'+cjsj+'\n到期时间：'+dqsj);
+    }
+}
+
+request(options, callback)};
+
+async function ica(){
+
+
+var headers = {
+    'authority': 'www.ddnsto.com',
+    'accept': 'application/json, text/plain, */*',
+    'x-csrftoken': csrftoken,
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'referer': 'https://www.ddnsto.com/app/',
+    'accept-language': 'zh-CN,zh;q=0.9,fr;q=0.8,ko;q=0.7',
+    'cookie': ddnstock
+};
+
+var options = {
+    url: 'https://www.ddnsto.com/api/user/product/orders/?product_expired_at__gt=2022-11-02+22:05:28',
+    headers: headers
+};
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        ui = body.match(/"id":\d+,"uid"/g)
+
+        ic= ui.toString().replace(',"uid"',"");
+        ia = ic.replace('"id":',"");
+        
+        
+        //console.log(ia);
+    }
+}
+
+
+request(options, callback)};
+
+async function uid(){
+var headers = {
+    'authority': 'www.ddnsto.com',
+    'accept': 'application/json, text/plain, */*',
+    'x-csrftoken': csrftoken,
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+    'content-type': 'application/json',
+    'origin': 'https://www.ddnsto.com',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'referer': 'https://www.ddnsto.com/app/',
+    'accept-language': 'zh-CN,zh;q=0.9,fr;q=0.8,ko;q=0.7',
+    'cookie': ddnstock
+};
+
+var dataString = '{"plan_ids_to_add":[${id}],"server":2}';
+
+var options = {
+    url: 'https://www.ddnsto.com/api/user/routers/'+ica+'/',
+    method: 'PATCH',
+    headers: headers,
+    body: dataString
+};
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      //  console.log(body);
+    }
+}
+
+request(options, callback)};
+
+
+
+async function ib(){
+  
+
+
+
+var headers = {
+    'authority': 'www.ddnsto.com',
+    'accept': 'application/json, text/plain, */*',
+    'x-csrftoken': csrftoken,
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'referer': 'https://www.ddnsto.com/app/',
+    'accept-language': 'zh-CN,zh;q=0.9,fr;q=0.8,ko;q=0.7',
+    'cookie': ddnstock
+};
+
+var options = {
+    url: 'https://www.ddnsto.com/api/user/product/orders/'+id+'/',
+    headers: headers
+};
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      cjsj = data.created_at
+      gxsj =data.updated_at
+      dqsj = data.product_expired_at
+      dqrz = data.product_expired_at
+      dglx = '4Mbps 7天试用'
+     
+
+        console.log('订购ddnsto：'+dglx+'\n创建时间：'+cjsj+'\n到期时间：'+dqsj);
+        console.log(body);
+    }
+}
+
+request(options, callback)};
+
+function Env(t, e) { class s { constructor(t) { this.env = t } send(t, e = "GET") { t = "string" == typeof t ? { url: t } : t; let s = this.get; return "POST" === e && (s = this.post), new Promise((e, i) => { s.call(this, t, (t, s, r) => { t ? i(t) : e(s) }) }) } get(t) { return this.send.call(this.env, t) } post(t) { return this.send.call(this.env, t, "POST") } } return new class { constructor(t, e) { this.name = t, this.http = new s(this), this.data = null, this.dataFile = "box.dat", this.logs = [], this.isMute = !1, this.isNeedRewrite = !1, this.logSeparator = "\n", this.startTime = (new Date).getTime(), Object.assign(this, e), this.log("", `\ud83d\udd14${this.name}, \u5f00\u59cb!`) } isNode() { return "undefined" != typeof module && !!module.exports } isQuanX() { return "undefined" != typeof $task } isSurge() { return "undefined" != typeof $httpClient && "undefined" == typeof $loon } isLoon() { return "undefined" != typeof $loon } toObj(t, e = null) { try { return JSON.parse(t) } catch { return e } } toStr(t, e = null) { try { return JSON.stringify(t) } catch { return e } } getjson(t, e) { let s = e; const i = this.getdata(t); if (i) try { s = JSON.parse(this.getdata(t)) } catch { } return s } setjson(t, e) { try { return this.setdata(JSON.stringify(t), e) } catch { return !1 } } getScript(t) { return new Promise(e => { this.get({ url: t }, (t, s, i) => e(i)) }) } runScript(t, e) { return new Promise(s => { let i = this.getdata("@chavy_boxjs_userCfgs.httpapi"); i = i ? i.replace(/\n/g, "").trim() : i; let r = this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout"); r = r ? 1 * r : 20, r = e && e.timeout ? e.timeout : r; const [o, h] = i.split("@"), a = { url: `http://${h}/v1/scripting/evaluate`, body: { script_text: t, mock_type: "cron", timeout: r }, headers: { "X-Key": o, Accept: "*/*" } }; this.post(a, (t, e, i) => s(i)) }).catch(t => this.logErr(t)) } loaddata() { if (!this.isNode()) return {}; { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e); if (!s && !i) return {}; { const i = s ? t : e; try { return JSON.parse(this.fs.readFileSync(i)) } catch (t) { return {} } } } } writedata() { if (this.isNode()) { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e), r = JSON.stringify(this.data); s ? this.fs.writeFileSync(t, r) : i ? this.fs.writeFileSync(e, r) : this.fs.writeFileSync(t, r) } } lodash_get(t, e, s) { const i = e.replace(/\[(\d+)\]/g, ".$1").split("."); let r = t; for (const t of i) if (r = Object(r)[t], void 0 === r) return s; return r } lodash_set(t, e, s) { return Object(t) !== t ? t : (Array.isArray(e) || (e = e.toString().match(/[^.[\]]+/g) || []), e.slice(0, -1).reduce((t, s, i) => Object(t[s]) === t[s] ? t[s] : t[s] = Math.abs(e[i + 1]) >> 0 == +e[i + 1] ? [] : {}, t)[e[e.length - 1]] = s, t) } getdata(t) { let e = this.getval(t); if (/^@/.test(t)) { const [, s, i] = /^@(.*?)\.(.*?)$/.exec(t), r = s ? this.getval(s) : ""; if (r) try { const t = JSON.parse(r); e = t ? this.lodash_get(t, i, "") : e } catch (t) { e = "" } } return e } setdata(t, e) { let s = !1; if (/^@/.test(e)) { const [, i, r] = /^@(.*?)\.(.*?)$/.exec(e), o = this.getval(i), h = i ? "null" === o ? null : o || "{}" : "{}"; try { const e = JSON.parse(h); this.lodash_set(e, r, t), s = this.setval(JSON.stringify(e), i) } catch (e) { const o = {}; this.lodash_set(o, r, t), s = this.setval(JSON.stringify(o), i) } } else s = this.setval(t, e); return s } getval(t) { return this.isSurge() || this.isLoon() ? $persistentStore.read(t) : this.isQuanX() ? $prefs.valueForKey(t) : this.isNode() ? (this.data = this.loaddata(), this.data[t]) : this.data && this.data[t] || null } setval(t, e) { return this.isSurge() || this.isLoon() ? $persistentStore.write(t, e) : this.isQuanX() ? $prefs.setValueForKey(t, e) : this.isNode() ? (this.data = this.loaddata(), this.data[e] = t, this.writedata(), !0) : this.data && this.data[e] || null } initGotEnv(t) { this.got = this.got ? this.got : require("got"), this.cktough = this.cktough ? this.cktough : require("tough-cookie"), this.ckjar = this.ckjar ? this.ckjar : new this.cktough.CookieJar, t && (t.headers = t.headers ? t.headers : {}, void 0 === t.headers.Cookie && void 0 === t.cookieJar && (t.cookieJar = this.ckjar)) } get(t, e = (() => { })) { t.headers && (delete t.headers["Content-Type"], delete t.headers["Content-Length"]), this.isSurge() || this.isLoon() ? (this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.get(t, (t, s, i) => { !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i) })) : this.isQuanX() ? (this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => e(t))) : this.isNode() && (this.initGotEnv(t), this.got(t).on("redirect", (t, e) => { try { if (t.headers["set-cookie"]) { const s = t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString(); this.ckjar.setCookieSync(s, null), e.cookieJar = this.ckjar } } catch (t) { this.logErr(t) } }).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => { const { message: s, response: i } = t; e(s, i, i && i.body) })) } post(t, e = (() => { })) { if (t.body && t.headers && !t.headers["Content-Type"] && (t.headers["Content-Type"] = "application/x-www-form-urlencoded"), t.headers && delete t.headers["Content-Length"], this.isSurge() || this.isLoon()) this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.post(t, (t, s, i) => { !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i) }); else if (this.isQuanX()) t.method = "POST", this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => e(t)); else if (this.isNode()) { this.initGotEnv(t); const { url: s, ...i } = t; this.got.post(s, i).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => { const { message: s, response: i } = t; e(s, i, i && i.body) }) } } time(t) { let e = { "M+": (new Date).getMonth() + 1, "d+": (new Date).getDate(), "H+": (new Date).getHours(), "m+": (new Date).getMinutes(), "s+": (new Date).getSeconds(), "q+": Math.floor(((new Date).getMonth() + 3) / 3), S: (new Date).getMilliseconds() }; /(y+)/.test(t) && (t = t.replace(RegExp.$1, ((new Date).getFullYear() + "").substr(4 - RegExp.$1.length))); for (let s in e) new RegExp("(" + s + ")").test(t) && (t = t.replace(RegExp.$1, 1 == RegExp.$1.length ? e[s] : ("00" + e[s]).substr(("" + e[s]).length))); return t } msg(e = t, s = "", i = "", r) { const o = t => { if (!t) return t; if ("string" == typeof t) return this.isLoon() ? t : this.isQuanX() ? { "open-url": t } : this.isSurge() ? { url: t } : void 0; if ("object" == typeof t) { if (this.isLoon()) { let e = t.openUrl || t.url || t["open-url"], s = t.mediaUrl || t["media-url"]; return { openUrl: e, mediaUrl: s } } if (this.isQuanX()) { let e = t["open-url"] || t.url || t.openUrl, s = t["media-url"] || t.mediaUrl; return { "open-url": e, "media-url": s } } if (this.isSurge()) { let e = t.url || t.openUrl || t["open-url"]; return { url: e } } } }; this.isMute || (this.isSurge() || this.isLoon() ? $notification.post(e, s, i, o(r)) : this.isQuanX() && $notify(e, s, i, o(r))); let h = ["", "==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="]; h.push(e), s && h.push(s), i && h.push(i), console.log(h.join("\n")), this.logs = this.logs.concat(h) } log(...t) { t.length > 0 && (this.logs = [...this.logs, ...t]), console.log(t.join(this.logSeparator)) } logErr(t, e) { const s = !this.isSurge() && !this.isQuanX() && !this.isLoon(); s ? this.log("", `\u2757\ufe0f${this.name}, \u9519\u8bef!`, t.stack) : this.log("", `\u2757\ufe0f${this.name}, \u9519\u8bef!`, t) } wait(t) { return new Promise(e => setTimeout(e, t)) } done(t = {}) { const e = (new Date).getTime(), s = (e - this.startTime) / 1e3; this.log("", `\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`), this.log(), (this.isSurge() || this.isQuanX() || this.isLoon()) && $done(t) } }(t, e) }
+
